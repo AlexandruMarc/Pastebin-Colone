30fb95727dd53fbd0868da1065f5499d422b2311
AlexandruMarc <marcalexander800@yahoo.com>
2024-07-29 16:51:08 +0300

    second

--- src/main/java/dev/alex/pastebin/PasteController.java
+++ src/main/java/dev/alex/pastebin/PasteController.java
@@ -20,9 +20,7 @@ public class PasteController {
     public String getAllPastes(Model model) {
         // Fetching all pastes from the service
         List<Paste> pastes = pasteService.getAllPastes();
-        // Adding the list of pastes to the model
         model.addAttribute("pastes", pastes);
-        // Returning the view name to display the paste list
         return "paste-list";
     }
 
@@ -31,20 +29,15 @@ public class PasteController {
     public String getPasteById(@PathVariable Long id, Model model) {
         // Fetching the paste by its ID, throwing an exception if not found
         Paste paste = pasteService.getPasteById(id).orElseThrow(() -> new IllegalArgumentException("Invalid paste Id:" + id));
-        // Adding the paste to the model
         model.addAttribute("paste", paste);
-        // Returning the view name to display the paste details
         return "paste-detail";
     }
 
     // Mapping to POST /pastes to save a new paste
     @PostMapping
     public String savePaste(@RequestParam String content) {
-        // Creating a new Paste object
         Paste paste = new Paste();
-        // Setting the content of the paste
         paste.setContent(content);
-        // Saving the paste using the service
         pasteService.savePaste(paste);
         // Redirecting to the list of pastes after saving
         return "redirect:/pastes";
